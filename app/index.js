6b0df35cbd7b8e4f96ea7af6b1216e48da5a9c8d
@maxgfr <maxgolfier@gmail.com>
2019-05-21 12:42:03 +0200

    push

--- app/index.js
+++ app/index.js
@@ -1,24 +1,85 @@
 const express = require('express')
 const cors = require('cors')
-const mariadb = require('mariadb');
+const bodyParser  = require('body-parser');
 const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const MongoUrl = 'mongodb://127.0.0.1:27017';
+const dbName = 'myproject';
+const dbCollection = 'documents';
 require('dotenv').config();
 
-const port = process.env.port || 3000
-const pool = mariadb.createPool({host: process.env.host || 'mydb.com', user: process.env.user || 'myUser', connectionLimit: 5});
+const port = process.env.port || 8000
 
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
+  MongoClient.connect(MongoUrl, function(err, client) {
+        if (err !== null) {
+            res.send('Could not connect to MongoDB');
+        } else {
+            res.send('Connected to MongoDB');
+            client.close();
+        }
+    });
+})
 
+app.get('/all', (req, res) => {
+  MongoClient.connect(MongoUrl, function(err, client) {
+      const db = client.db(dbName);
+      // Get the documents collection
+      const collection = db.collection(dbCollection);
+      // Find document
+      collection.find({}).toArray(function(err, docs) {
+        if(!err) {
+          res.json(docs);
+        } else {
+          res.json({find: false, error: true});
+        }
+      });
+      client.close();
+    });
 })
 
 app.post('/', (req, res) => {
-
+    MongoClient.connect(MongoUrl, function(err, client) {
+        const db = client.db(dbName);
+        // Get the documents collection
+        const collection = db.collection(dbCollection);
+        // Insert some documents
+        if(req.body) {
+          collection.insertOne(req.body, function(err, result) {
+            if(!err) {
+              res.json({added: true, error: false});
+            } else {
+              res.json({added: false, error: true});
+            }
+          });
+        } else {
+          res.json({error: true});
+        }
+        client.close();
+    });
 })
 
 app.delete('/', (req, res) => {
-
+    MongoClient.connect(MongoUrl, function(err, client) {
+      const db = client.db(dbName);
+      // Get the documents collection
+      const collection = db.collection(dbCollection);
+      if(req.body)  {
+        collection.deleteOne(req.body, function(err, result) {
+          if(!err) {
+            res.json({deleted: true, error: false});
+          } else {
+            res.json({deleted: false, error: true});
+          }
+        });
+      } else {
+        res.json({error: true});
+      }
+      client.close();
+    });
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
