1e9696cce7773205a3544b918edfaa7155be93be
@maxgfr <maxgolfier@gmail.com>
2019-05-22 10:45:20 +0200

    push

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ docker-compose up
 ```bash
 docker-compose down
 ```
+
+zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties
+kafka-server-start /usr/local/etc/kafka/server.properties
+kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
+kafka-console-producer --broker-list localhost:9092 --topic test
+kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning
--- index.js
+++ index.js
@@ -1,42 +1,104 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser  = require('body-parser');
+const util = require('util');
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
-
+const kafka = require('kafka-node');
 require('dotenv').config();
-
-const MongoUrl = process.env.MONGO_URL || 'mongodb://mongo:27017';
+// mongodb://127.0.0.1:27017 for local or mongodb://mongo:27017 for docker
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017';
 const dbName = process.env.DB_NAME || 'myproject';
 const dbCollection = process.env.DB_COLLECTION || 'documents';
+const port = process.env.PORT || 3000;
+const kafkaUrl = process.env.KAFKA_URL || 'http://localhost:2181';
+const mainTopic = [{
+  topic: dbName
+}];
+const kafkaOptions = {
+  autoCommit: true,
+  fetchMaxWaitMs: 1000,
+  fetchMaxBytes: 1024 * 1024,
+  encoding: "buffer"
+};
+const kafkaClient = new kafka.KafkaClient(kafkaUrl);
+const kafkaProducer = new kafka.HighLevelProducer(kafkaClient);
+const kafkaConsumer = new kafka.Consumer(kafkaClient, mainTopic, kafkaOptions);
 
-const port = process.env.PORT || 3000
+kafkaProducer.on("ready", function() {
+    console.log("Kafka Producer is connected and ready.");
+});
+
+kafkaProducer.on("error", function(error) {
+    console.error(error);
+});
+
+kafkaConsumer.on("message", function(message) {
+    console.log('kafka consumer :')
+    console.log('<=====================>')
+    var buf = new Buffer(message.value, "binary");
+    var decodedMessage = JSON.parse(buf.toString());
+    console.log(util.inspect(decodedMessage, false, null, true));
+    console.log('<=====================>')
+});
+
+kafkaConsumer.on("error", function(err) {
+    console.log("error", err);
+});
+
+kafkaConsumer.on("SIGINT", function() {
+    kafkaConsumer.close(true, function() {
+        console.log('kafka is close');
+    });
+});
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  MongoClient.connect(MongoUrl, function(err, client) {
+  MongoClient.connect(mongoUrl, function(err, client) {
         if (err !== null) {
-            res.send('Could not connect to MongoDB');
+            res.json({message: 'Could not connect to MongoDB', error: true});
         } else {
-            res.send('Connected to MongoDB');
+            kafkaClient.createTopics(mainTopic, (error, result) => {
+              if(!error) {
+                console.log(result)
+                res.json({message: 'Connected to MongoDB and KafkaClient', error: false})
+              } else {
+                res.json({message: 'Error with KafkaClient', error: true});
+              }
+            });
             client.close();
         }
     });
 })
 
 app.get('/all', (req, res) => {
-  MongoClient.connect(MongoUrl, function(err, client) {
+  MongoClient.connect(mongoUrl, function(err, client) {
       const db = client.db(dbName);
       // Get the documents collection
       const collection = db.collection(dbCollection);
       // Find document
       collection.find({}).toArray(function(err, docs) {
         if(!err) {
-          res.json(docs);
+          const buffer = new Buffer.from(JSON.stringify(docs));
+          const record = [
+              {
+                  topic: dbName,
+                  messages: buffer,
+                  attributes: 1 /* Use GZip compression for the payload */
+              }
+          ];
+          kafkaProducer.send(record, function (error, data) {
+              if(!error) {
+                console.log(data);
+                res.json(docs);
+              } else {
+                res.json({message: 'error with kafka', error: true});
+              }
+          });
         } else {
-          res.json({find: false, error: true});
+          res.json({message: 'error with mongo', error: true});
         }
       });
       client.close();
@@ -44,7 +106,7 @@ app.get('/all', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    MongoClient.connect(MongoUrl, function(err, client) {
+    MongoClient.connect(mongoUrl, function(err, client) {
         const db = client.db(dbName);
         // Get the documents collection
         const collection = db.collection(dbCollection);
@@ -52,33 +114,63 @@ app.post('/', (req, res) => {
         if(req.body) {
           collection.insertOne(req.body, function(err, result) {
             if(!err) {
-              res.json({added: true, error: false});
+              const buffer = new Buffer.from(JSON.stringify([{data: req.body}]));
+              const record = [
+                  {
+                      topic: dbName,
+                      messages: buffer,
+                      attributes: 1 /* Use GZip compression for the payload */
+                  }
+              ];
+              kafkaProducer.send(record, function (error, data) {
+                  if(!error) {
+                    console.log(data);
+                    res.json({message: 'added to the database and kafka', error: false});
+                  } else {
+                    res.json({message: 'error with kafka', error: true});
+                  }
+              });
             } else {
-              res.json({added: false, error: true});
+              res.json({message: 'error with mongo', error: true});
             }
           });
         } else {
-          res.json({error: true});
+          res.json({message: 'error with the body of the request', error: true});
         }
         client.close();
     });
 })
 
 app.delete('/', (req, res) => {
-    MongoClient.connect(MongoUrl, function(err, client) {
+    MongoClient.connect(mongoUrl, function(err, client) {
       const db = client.db(dbName);
       // Get the documents collection
       const collection = db.collection(dbCollection);
       if(req.body)  {
         collection.deleteOne(req.body, function(err, result) {
           if(!err) {
-            res.json({deleted: true, error: false});
+            const buffer = new Buffer.from(JSON.stringify([{data: req.body}]));
+            const record = [
+                {
+                    topic: dbName,
+                    messages: buffer,
+                    attributes: 1 /* Use GZip compression for the payload */
+                }
+            ];
+            kafkaProducer.send(record, function (error, data) {
+                if(!error) {
+                  console.log(data);
+                  res.json({message: 'remove to the database and message added to kafka', error: false});
+                } else {
+                  res.json({message: 'error with kafka', error: true});
+                }
+            });
           } else {
-            res.json({deleted: false, error: true});
+            res.json({message: 'error with mongo', error: true});
           }
         });
       } else {
-        res.json({error: true});
+        res.json({message: 'error with the body of the request', error: true});
       }
       client.close();
     });
